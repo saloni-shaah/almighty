fd77c5d38a4e4cb2634eaf4ba8a61145d9ce6299
Bhaskar Shah <next181489111@gmail.com>
2025-09-18 09:50:53 +0000

    no the bacground did not changed please change it please like this make

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import { Chat } from "@/components/chat/chat";
 export default function Home() {
   return (
     <div
-      className="flex h-full w-full items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url(https://storage.googleapis.com/aai-web-samples/app-prototyper/bg.jpg)",
-      }}
+      className="flex h-full w-full items-center justify-center"
     >
       <main className="flex h-full w-full items-center justify-center p-4 md:p-6">
         <Chat />
